55f3ed6f2d80976d31b9dbc8647237d9b41c5ac0
balnelius19 <balnelius19@gmail.com>
2025-11-05 12:50:11 +0100

    mise a jour du css et du html

--- connexion.php
+++ connexion.php
@@ -7,13 +7,24 @@
     <meta name="author" content="groupe 3 les blasés du blason">
     <title>Connectez vous a squizzel run</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="connexion.css">
 </head>
 
 <body class="BGBlue margin1">
     <h1 class="white">|Se connecter</h1>
-    <div class="center rectangle30">
-        <form>
-
+    <div class="center flex t100">
+        <form action="" method="post" class="center t100 h90vh wrap margin0">
+            <div class="column t50 ">
+                <label for="email" class="left white size10  ">Email :</label>
+                <input type="email" id="email" name="email" class="t100 BGMediumBlue marginTop-10" placeholder="email" required>
+            </div>
+            <div class="column t50 ">
+                <label for="password" class="t100 left white size10 margin0">mot de passe :</label>
+                <input type="password" id="password" name="password" class="t100 BGMediumBlue marginTop-10" placeholder="mot de passe"required>
+            </div>
+            <div class="column t50 margin1">
+                <button type="submit" class="t15 marginRight85 size10">Envoyer</button>
+            </div>
         </form>
     </div>
 </body>
