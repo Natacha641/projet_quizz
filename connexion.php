367e795db3e2e8a1e463dc548c4ba64871e11bd9
balnelius19 <balnelius19@gmail.com>
2025-11-05 10:57:49 +0100

    ajout fonctions css et titre html

--- connexion.php
+++ connexion.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,14 @@
     <title>Connectez vous a squizzel run</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-    
+
+<body class="BGBlue margin1">
+    <h1 class="white">|Se connecter</h1>
+    <div class="center rectangle30">
+        <form>
+
+        </form>
+    </div>
 </body>
+
 </html>
