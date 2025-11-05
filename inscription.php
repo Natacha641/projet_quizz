9b4e046ebb49b1a2bdbf42b177e35a7e74b3fb00
balnelius19 <balnelius19@gmail.com>
2025-11-05 15:32:20 +0100

    maj police

--- inscription.php
+++ inscription.php
@@ -8,6 +8,9 @@
     <title>Inscrivez vous a squizzel run</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="connexion.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ballet:opsz@16..72&family=Baumans&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Gemstones&display=swap" rel="stylesheet">
 </head>
 
 <body class="BGBlue margin1">
@@ -38,7 +41,7 @@
                 <a href="connexion.php" class="lien lightBlue">j'ai déja un compte</a>
             </div>
             <div class="button margin1">
-                <button type="submit" class="BGOrange" name="envoyer">Envoyer</button>
+                <button type="submit" class="BGOrange size12" name="envoyer">Valider l'inscription</button>
             </div>
         </form>
     </div>
